43cdd8c10fb77e12a34dcc27e21b9d6f0ded816a
Subhrodip Mohanta <subhrodipmohanta@gmail.com>
2020-05-10 20:05:45 +0530

    Spring Application Running

---
+++ covid-tracker/src/main/java/xyz/subho/covidtracker/controllers/MainController.java
@@ -0,0 +1,32 @@
+package xyz.subho.covidtracker.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import xyz.subho.covidtracker.models.LocationStats;
+import xyz.subho.covidtracker.services.CoronaVirusDataService;
+
+@Controller
+public class MainController {
+
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+
+        return "index";
+    }
+}
