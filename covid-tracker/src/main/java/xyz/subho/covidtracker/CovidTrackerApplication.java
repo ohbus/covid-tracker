747345217ded0e9739212280b2271ee114f6c82d
Subhrodip Mohanta <subhrodipmohanta@gmail.com>
2020-05-10 17:59:34 +0530

    Initialization of Spring Boot Application

---
+++ covid-tracker/src/main/java/xyz/subho/covidtracker/CovidTrackerApplication.java
@@ -0,0 +1,13 @@
+package xyz.subho.covidtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidTrackerApplication.class, args);
+	}
+
+}
