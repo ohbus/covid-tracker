8c1a7d0b8c12ec8a5c3b12e89cd30371d78c99b0
Priya Srivastava <priya.srivastava@thougtworks.com>
2021-01-11 09:54:23 +0530

    Added Test cases and Refactored CovidTracker Service

--- covid-tracker/src/main/java/xyz/subho/covidtracker/services/CoronaVirusDataService.java
+++ covid-tracker/src/main/java/xyz/subho/covidtracker/services/CoronaVirusDataService.java
@@ -3,25 +3,23 @@ package xyz.subho.covidtracker.services;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
+import xyz.subho.covidtracker.client.CoronaVirusDataClient;
 import xyz.subho.covidtracker.models.LocationStats;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.io.StringReader;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CoronaVirusDataService {
 
-    private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+    @Autowired
+    private CoronaVirusDataClient coronaVirusDataClient;
 
     private List<LocationStats> allStats = new ArrayList<>();
 
@@ -29,39 +27,38 @@ public class CoronaVirusDataService {
         return allStats;
     }
 
+    CoronaVirusDataService(CoronaVirusDataClient coronaVirusDataClient)
+    {
+        this.coronaVirusDataClient = coronaVirusDataClient;
+    }
+
     @PostConstruct
     @Scheduled(cron = "0 0/30 * * * *")
     public void fetchVirusData() throws IOException, InterruptedException {
         List<LocationStats> newStats = new ArrayList<>();
-        
-        HttpClient client = HttpClient.newHttpClient();
-        
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(VIRUS_DATA_URL))
-                .build();
-        
-        HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
-        
-        StringReader csvBodyReader = new StringReader(httpResponse.body());
-        
+
+        StringReader csvBodyReader = new StringReader(coronaVirusDataClient.getVirusData());
+
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
-        
-        for (CSVRecord record : records) {
-            LocationStats locationStat = new LocationStats();
-            
-            locationStat.setState(record.get("Province/State"));
-            locationStat.setCountry(record.get("Country/Region"));
-            
-            int latestCases = Integer.parseInt(record.get(record.size() - 1));
-            int prevDayCases = Integer.parseInt(record.get(record.size() - 2));
-            
-            locationStat.setLatestTotalCases(latestCases);
-            locationStat.setDiffFromPrevDay(latestCases - prevDayCases);
-            
-            newStats.add(locationStat);
-        }
-        
+
+        readCSV(newStats, records);
         this.allStats = newStats;
     }
 
+    private void readCSV(List<LocationStats> newStats, Iterable<CSVRecord> records) {
+        for (CSVRecord record : records) {
+            LocationStats locationStat = new LocationStats();
+
+            locationStat.setState(record.get("Province/State"));
+            locationStat.setCountry(record.get("Country/Region"));
+
+            int latestCases = Integer.parseInt(record.get(record.size() - 1));
+            int prevDayCases = Integer.parseInt(record.get(record.size() - 2));
+
+            locationStat.setLatestTotalCases(latestCases);
+            locationStat.setDiffFromPrevDay(latestCases - prevDayCases);
+
+            newStats.add(locationStat);
+        }
+    }
 }
